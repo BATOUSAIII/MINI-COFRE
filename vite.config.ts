3c0010eb1a2875b7cbf56ab86000e396c951c5b3
Pedro Henrique <133808968+BATOUSAIII@users.noreply.github.com>
2026-06-20 05:40:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/MINI-COFRE/', // 👈 ADICIONE ESTA LINHA AQUI
       server: {
         port: 3000,
         host: '0.0.0.0',
